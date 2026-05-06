e7e1b6d36f0db7c00c92a8b8fad0628880cd1f34
Laptop USA Pro <24130130@st.hcmuaf.edu.vn>
2026-05-06 14:55:26 +0700

    Tạo giao diện mục tiêu

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     private CardLayout cardLayout;
     private JPanel contentPanel;
+    private GoalPanel goalPanel;
 
     private JButton btnTapTrung;
     private JButton btnQuanLyBaiTap;
@@ -68,7 +69,8 @@ public class MainFrame extends JFrame {
 
         contentPanel.add(createPlaceholderPanel("Màn hình Tập trung Pomodoro"), "TapTrung");
         contentPanel.add(createPlaceholderPanel("Màn hình Quản lý bài tập"), "QuanLyBaiTap");
-        contentPanel.add(createPlaceholderPanel("Màn hình Mục tiêu"), "MucTieu");
+        goalPanel = new GoalPanel();
+        contentPanel.add(goalPanel, "MucTieu");
         contentPanel.add(createPlaceholderPanel("Màn hình Thống kê"), "ThongKe");
         contentPanel.add(createPlaceholderPanel("Màn hình Hồ sơ"), "HoSo");
 
